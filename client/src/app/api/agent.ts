55908fdb39997889b1f2cf48f3a608d8948e4a7f
Ashley Staples <ashley.d.staples@gmail.com>
2024-03-21 11:43:27 +1000

    EndofLesson53

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -17,6 +17,13 @@ axios.interceptors.response.use(response => {
     const {data, status} = error.response as AxiosResponse;
     switch (status) {
         case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    modelStateErrors.push(data.errors[key])
+                }
+                throw modelStateErrors.flat();
+            }
             toast.error(data.title);
             break;
         case 401:
